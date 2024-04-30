03d33ed2aeb3607e96e745e958deab251d01088c
Вадим ИВТ-211 <rentsvadim@gmail.com>
2024-04-30 15:07:50 +0300

    Добавьте файлы проекта.

---
+++ Module_9_HW/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class CustomException : Exception
+{
+    public CustomException(string message) : base($"Слово \"{message}\" содержит букву 'А'.")
+    {
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        try
+        {
+            string str = "автобус";
+            CheckForLetterA(str);
+            Console.WriteLine(str);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Ошибка деления на ноль.");
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Ошибка выхода за границы массива.");
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("Ошибка нулевого указателя (null).");
+        }
+        catch (InvalidOperationException)
+        {
+            Console.WriteLine("Ошибка недопустимой операции.");
+        }
+        catch (CustomException)
+        {
+            Console.WriteLine($"В слове имеется буква заглавная А");
+        }
+        finally
+        {
+                Console.WriteLine("Блок finally выполнен.");
+        }
+    }
+
+    static void CheckForLetterA(string word)
+    {
+        if (word.Contains("А"))
+        {
+            throw new CustomException("Слово содержит букву 'А'.");
+        }
+    }
+}
